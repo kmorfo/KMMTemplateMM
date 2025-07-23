dffd851f5780e696126e5623ca9f448fd860300d
Raúl Luján Colilla <mcdrokz@gmail.com>
2025-07-23 18:58:51 +0200

    Update Dependencies
    Update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,17 +3,14 @@ import org.gradle.kotlin.dsl.libs
 import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
 
 plugins {
-    // this is necessary to avoid the plugins to be loaded multiple times
-    // in each subproject's classloader
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.androidLibrary) apply false
-    alias(libs.plugins.composeMultiplatform) apply false
     alias(libs.plugins.composeCompiler) apply false
-    alias(libs.plugins.kotlinMultiplatform) apply false
+    alias(libs.plugins.composeMultiplatform) apply false
     alias(libs.plugins.jetbrainsKotlinJvm) apply false
+    alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.kotlinxSerialization) apply false
     alias(libs.plugins.ksp) apply false
-
     alias(libs.plugins.ktlint) apply true
 }
 
